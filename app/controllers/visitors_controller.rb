e4caa7581848b34d49afbaaa635c06e12349b83a
MartinCheong <martin.cej90@gmail.com>
2016-03-09 14:43:05 +0800

    added front-end framework : foundation

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,10 +1,7 @@
 class VisitorsController < ApplicationController
 
 	def new
-	  Rails.logger.debug 'DEBUG: entering new method'
 	  @owner = Owner.new
-	  flash.now[:notice] = 'Welcome!'
-	  flash.now[:alert]  = 'My Birthday is Soon'
  	 
 	end
 
